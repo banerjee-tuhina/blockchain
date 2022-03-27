fc0101f45b200dc1d453227edab9aed12134e46b
tuhina.jayanta <tuhina.jayanta@gmail.com>
2022-03-27 21:28:43 +0530

    First commit

---
+++ test.py
@@ -0,0 +1,10 @@
+#import blockchain
+#bc=blockchain.Blockchain()
+#first_data=bc.chain
+#print(first_data)
+#mine=bc.mine_block(data=input('ask the user?'))
+#print(mine)
+#create=bc._create_block()
+#print(create)
+a=bool(1)
+print(str(a))
